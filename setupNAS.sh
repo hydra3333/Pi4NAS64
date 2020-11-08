4e5d3e3f7e1eb1036b374a463e466dd7e6eaf3f7
hydra3333 <hydra3333@gmail.com>
2020-11-08 13:56:19 +1030

    Add files via upload

--- setupNAS.sh
+++ setupNAS.sh
@@ -2,20 +2,9 @@
 # to get rid of MSDOS format do this to this file: sudo sed -i.bak s/\\r//g ./filename
 # or, open in nano, control-o and then then alt-M a few times to toggle msdos format off and then save
 #
-
-echo "############# UNDER CONSTRUCTION"
-echo "############# UNDER CONSTRUCTION"
-echo "############# UNDER CONSTRUCTION"
-echo "############# UNDER CONSTRUCTION"
-echo "############# UNDER CONSTRUCTION"
-echo "############# UNDER CONSTRUCTION"
-exit
-
-
 set -x
 cd ~/Desktop
 set +x
-
 #-------------------------------------------------------------------------------------------------------------------------------------
 echo ""
 echo "# Have we completed the pre-Install set of instructions per the Readme ?"
@@ -727,6 +716,7 @@ echo "# If the cleanup did not work, control-C then fix any issues, then re-star
 read -p "# Otherwise - Press Enter to continue."
 echo ""
 echo "################################################################################################################################"
+echo ""
 #-------------------------------------------------------------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------------------------------------------------------------
@@ -898,6 +888,7 @@ echo "# If something did not work, control-C then fix any issues, then re-start 
 read -p "# Otherwise - Press Enter to continue."
 echo ""
 echo "################################################################################################################################"
+echo ""
 #-------------------------------------------------------------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------------------------------------------------------------
@@ -1161,22 +1152,113 @@ echo ""
 echo "################################################################################################################################"
 #-------------------------------------------------------------------------------------------------------------------------------------
 
-
-
-
-
-
 #-------------------------------------------------------------------------------------------------------------------------------------
 echo ""
-echo "# Install proFTPd set server and ocnfigure it"
+echo "################################################################################################################################"
+echo ""
+echo "# Install proFTPd set server and configure it. It is connectable from filezilla"
+echo ""
 read -p "# Press Enter to continue."
+echo ""
+echo "# Un-Install then Install any prior proFTPd"
+echo ""
+set -x
+cd ~/Desktop
+sudo kill -TERM `cat /run/proftpd.pid`
+sudo rm -fv "/etc/shutmsg"
+sudo apt purge -y proftpd proftpd-basic proftpd-mod-case proftpd-doc 
+sudo chmod -c a=rwx -R "/etc/proftpd/proftpd.conf"
+sudo rm -vf "/etc/proftpd/proftpd.conf" "/etc/proftpd/proftpd.conf.old"
+sudo apt install -y proftpd proftpd-mod-case proftpd-doc 
+sudo cat /var/log/proftpd/proftpd.log
+set +x
+echo ""
+echo "# If something did not work, control-C then fix any issues, then re-start this script."
+read -p "# Otherwise - Press Enter to continue."
+echo ""
+##
+echo ""
+echo "# Configure proFTPd"
+echo ""
+# https://htmlpreview.github.io/?https://github.com/Castaglia/proftpd-mod_case/blob/master/mod_case.html
+# Use directives
+#  CaseIgnore on
+#  LoadModule mod_case.c
+# to enable case-insensitivity for all FTP commands handled by mod_case
+# http://www.proftpd.org/docs/howto/ServerType.html
+# http://www.proftpd.org/docs/howto/Stopping.html
+# update the conf file
+set -x
+# http://www.proftpd.org/docs/howto/Stopping.html
+# deny incoming connections - does not stop the ftp server
+sudo ftpshut -l 0 -d 0 now
+# now kill the daemon
+sudo kill -TERM `cat /run/proftpd.pid`
+#
+sudo rm -fv "/etc/proftpd/proftpd.conf.old"
+sudo cp -fv "/etc/proftpd/proftpd.conf" "/etc/proftpd/proftpd.conf.old" 
+rm -fv "./tmp.tmp"
+cat<<EOF >"./tmp.tmp"
+# Case Sensitive module at top
+LoadModule mod_case.c
+<IfModule mod_case.c>
+CaseIgnore on
+</IfModule>
+EOF
+sudo cat "/etc/proftpd/proftpd.conf">>"./tmp.tmp"
+#
+sudo sed -i "s;ServerName\t\t\t\"Debian\";ServerName\t\t\t\"Pi4CC\";g" "./tmp.tmp"
+sudo sed -i "s;DisplayLogin;#DisplayLogin;g" "./tmp.tmp"
+sudo sed -i "s;DisplayChdir;#DisplayChdir;g" "./tmp.tmp"
+sudo sed -i "s;# RequireValidShell\t\toff;RequireValidShell\t\toff;g" "./tmp.tmp"
+sudo sed -i "s;User\t\t\t\tproftpd;User\t\t\t\tpi;g" "./tmp.tmp"
+sudo sed -i "s;Group\t\t\t\tnogroup;Group\t\t\t\tpi;g" "./tmp.tmp"
+sudo sed -i "s;Group\t\t\t\tpi;Group\t\t\t\tpi\nDefaultRoot \~ \!pi,\!www-data;g" "./tmp.tmp"
+sudo sed -i "s;Umask\t\t\t\t022  022;Umask\t\t\t\t000  000;g" "./tmp.tmp"
+#"/boot_delay/d"
+sudo sed -i "/# Include other custom configuration files/d" "./tmp.tmp"
+sudo sed -i "/Include \/etc\/proftpd\/conf.d\//d" "./tmp.tmp"
+#cat<<EOF >>"./tmp.tmp"
+#<Anonymous ~pi>
+#User pi
+#Group pi
+#DefaultRoot ~ !pi,!www-data
+#UserAlias anonymous pi
+#RequireValidShell off
+#MaxClients 30
+#Umask 000 000
+#<Directory *>
+#Umask 000 000
+#<Limit WRITE>
+#AllowAll
+#</Limit>
+#</Directory>
+#</Anonymous>
+# Include other custom configuration files
+#Include /etc/proftpd/conf.d/
+#EOF
+#cat "./tmp.tmp"
+#
+sudo cp -fv "./tmp.tmp" "/etc/proftpd/proftpd.conf"
+rm -f "./tmp.tmp"
+ls -al "/etc/proftpd/proftpd.conf.old" "/etc/proftpd/proftpd.conf"
+sudo diff -U 10 "/etc/proftpd/proftpd.conf.old" "/etc/proftpd/proftpd.conf"
+# re-enable server
+sudo kill -TERM `cat /run/proftpd.pid`
+sudo rm -fv "/etc/shutmsg"
+sudo proftpd
+#
+sudo proftpd -t -d5
+#sudo proftpd -vv
+#sudo proftpd --list 
+set +x
 echo ""
 echo "# If that did not work, control-C then fix any issues, then re-start this script."
 read -p "# Otherwise - Press Enter to continue."
 echo ""
+echo "################################################################################################################################"
+echo ""
 #-------------------------------------------------------------------------------------------------------------------------------------
-
-
 
 echo ""
 echo "# Please Reboot the Pi4 now for the updated settings to take effect"

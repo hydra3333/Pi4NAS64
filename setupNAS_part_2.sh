797aab27535f9d98e8616fccf2a960b7a3c2df0f
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-02-15 13:08:15 +1030

    x

--- setupNAS_part_2.sh
+++ setupNAS_part_2.sh
@@ -896,8 +896,10 @@ set -x
 cd ~/Desktop
 sudo rm -vf "./local_crontab.txt"
 crontab -l > "./local_crontab.txt"
+
 sed -i "/no crontab for $(whoami)/d" "./local_crontab.txt"
-EscapedPath=(echo ${minidlna_refresh_sh_file} | sed 's:/:\\\/:g')	# escaped path for use in: sed "/findstring/d"
+EscapedPath=(echo "${minidlna_refresh_sh_file}" | sed 's:/:\\\/:g')	
+# escaped path for use in: sed "/findstring/d"
 
 cat "./local_crontab.txt"
 sed -i "/${EscapedPath}/d" "./local_crontab.txt"

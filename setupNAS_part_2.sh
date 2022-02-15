569c16b87952f5bf9beeef824fa9d271022117e0
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-02-15 13:26:39 +1030

    x

--- setupNAS_part_2.sh
+++ setupNAS_part_2.sh
@@ -896,26 +896,18 @@ set -x
 cd ~/Desktop
 sudo rm -vf "./local_crontab.txt"
 crontab -l > "./local_crontab.txt"
-
 sed -i "/no crontab for $(whoami)/d" "./local_crontab.txt"
-#EscapedPath=(echo "${minidlna_refresh_sh_file}" | sed 's:/:\\\/:g')
 # escaped path for use in: sed "/findstring/d"
 EscapedPath=`echo "${minidlna_refresh_sh_file}" | sed 's:/:\\\/:g'`
 cat "./local_crontab.txt"
 sed -i "/${EscapedPath}/d" "./local_crontab.txt"
+
 cat "./local_crontab.txt"
-
-read -p "# Press Enter to continue."
-
-
-echo "0 2 * * * ${minidlna_refresh_sh_file} \2\>\&1 \>\> ${minidlna_refresh_log_file}" >> "./local_crontab.txt"
+echo "0 2 * * * ${minidlna_refresh_sh_file} 2>&1 >> ${minidlna_refresh_log_file}" >> "./local_crontab.txt"
 cat "./local_crontab.txt"
-
 
 sort "./local_crontab.txt" | uniq > "./local_crontab.txt"
 cat "./local_crontab.txt"
-
-
 crontab "./local_crontab.txt"
 rm -vf "./local_crontab.txt"
 set +x
